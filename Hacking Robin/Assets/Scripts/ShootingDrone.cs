f25ba3426a4c43fe297a452d6054b0f7294c060c
MiNeves00 <miguel.neves.filipe@gmail.com>
2022-03-27 12:14:05 +0100

    Shooting Drone now lives after some seconds

--- Hacking Robin/Assets/Scripts/ShootingDrone.cs
+++ Hacking Robin/Assets/Scripts/ShootingDrone.cs
@@ -9,12 +9,17 @@ public class ShootingDrone : MonoBehaviour
     public float xspeed;
     public GameObject player;
     public GameObject drone;
+    public int timeStopped; // time in seconds it stops in air before leaving
+    private bool stop = false;
+    private bool stoppedOnce = false; // true if has stopped once
+    private float original_xspeed; // copy of original xspeed
 
     // Use this for initialization
     void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D> ();
+        original_xspeed = xspeed;
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -22,17 +27,27 @@ public class ShootingDrone : MonoBehaviour
     {
 
 
-	if (drone.transform.position.x - player.transform.position.x < 20 && drone.transform.position.x - player.transform.position.x > 0){
+	if (drone.transform.position.x - player.transform.position.x < 20 && drone.transform.position.x - player.transform.position.x > 0 && stop == false && stoppedOnce == false){
 		xspeed = 10;
-		Debug.Log("STOP");
+		stop = true;
+		stoppedOnce = true;
 	}
-	//if(drone.transform.position.x - player.transform.position.x > 0)
-	//	Debug.Log(drone.transform.position.x - player.transform.position.x);
+	if(stop == true){
+		StartCoroutine(stopWait());
+	}
 	
 		Vector2 newVelocity = rb2d.velocity;
 		newVelocity.x = xspeed;
 		rb2d.velocity = newVelocity;
 
+    }
+    
+
+    IEnumerator stopWait()
+    {
+		yield return new WaitForSeconds(timeStopped);
+		xspeed = original_xspeed;
+		stop = false;
     }
     
     
