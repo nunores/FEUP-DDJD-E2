16f775e7dfa38d637a8210847ec3c8cc56ad19aa
João Gonçalves <50209733+SouOCalves@users.noreply.github.com>
2022-03-30 12:40:04 +0100

    Backgrounds of feup added

--- Hacking Robin/Assets/Scripts/InfiniteScrolling.cs
+++ Hacking Robin/Assets/Scripts/InfiniteScrolling.cs
@@ -9,6 +9,7 @@ public class InfiniteScrolling : MonoBehaviour
     public GameObject[] possibleBackgrounds;
     public GameObject[] possibleObstacles;
     public GameObject[] possibleGoodThings;
+    public GameObject pillar;
     public GameObject player;
     public GameObject currentRoom;
     public GameObject currentObstacle;
@@ -32,7 +33,7 @@ public class InfiniteScrolling : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        backgroundWidth = 29.56f; //TODO: change this value when we change the sprite (hardcoded)
+        backgroundWidth = 28.8f; //TODO: change this value when we change the sprite (hardcoded)
         coinCount = 0;
         numberActiveCoffee = 0;
         numberActiveBeer = 0;
@@ -65,8 +66,10 @@ public class InfiniteScrolling : MonoBehaviour
 
     private GameObject generateBackground(){
         GameObject room = (GameObject)Instantiate(possibleBackgrounds[Random.Range(0, possibleBackgrounds.Length)]);
+        GameObject newPillar = (GameObject)Instantiate(pillar);
         room.SetActive(true);
         room.transform.position = new Vector3(backgroundWidth * (backgroundNumber + 1), 0, 0);
+        pillar.transform.position = new Vector3((backgroundWidth * (backgroundNumber + 1)) - (backgroundWidth / 2), 0, 0);
         return room;
     }
 
